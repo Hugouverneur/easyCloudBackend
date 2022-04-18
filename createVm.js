4981c848484cdc13de50e9461d66e1798d55886a
Hugo <hugo.umbert@estiam.com>
2022-04-18 13:17:27 +0200

    Add powershell command

--- createVm.js
+++ createVm.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const powershell = require('node-powershell');
+const { exec } = require('child_process');
 
 function createRouter() {
     const router = express.Router();
 
+    // Powershell command test by creating file with vmName
     router.post('/createvm', async (req, res, next) => {
         console.log(req.body);
 
-        const ps = new powershell.PowerShell();
+        // Create Powershell command
+        let commandInput = `Write-Output "Creating file ...";
+            New-Item -Path . -Name "${req.body.vmName}.txt" -ItemType "file" -Value "Created with nodejs";
+            Write-Output "File created !"`;
 
-        await ps.invoke(
-            powershell.PowerShell.command`Write-Output 'VM name: ${req.body.vmName} with ${req.body.ram}${req.body.unity} and ${req.body.storage}${req.body.unity} running on ${req.body.os}`
-        )
-        await ps.dispose();
-        // // Start the process
-        // let ps = new PowerShell(
-        //     "Write-Output 'My vm :'"
-        //     // `Write-Output 'VM name: ${req.body.vmName} with ${req.body.ram}${req.body.unity} and ${req.body.storage}${req.body.unity} running on ${req.body.os}`
-        //     );
-        // // Handle process errors (e.g. powershell not found)
-        // ps.on("error", err => {
-        //     console.error(err);
-        // });
-        // // Stdout
-        // ps.on("output", data => {
-        //     console.log(data);
-        // });
+        // Exec command
+        exec(commandInput, {'shell':'powershell.exe'}, (error, data) => {
+            if(error) {
+                throw error;
+            } else {
+                console.log(`${data}`);
+            }
+        });
     });
 
     return router;
