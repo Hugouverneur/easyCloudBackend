951f7318aa1da43269f108475bf03dd924384586
Mathieu <mathieu.ranieri@outlook.fr>
2022-09-11 16:03:11 +0200

    Create PS Exec function, adding routes for configuration and monitoring

--- manageVm.js
+++ manageVm.js
@@ -6,89 +6,118 @@ const Logger = require('nodemon/lib/utils/log');
 function createRouter() {
     const router = express.Router();
 
-    //Qui serait exécuté sur la page du formulaire de création de la VM
-    router.get('/initcreation', async (req, res, next) => {
-        let proc = exec("Get-AvailableIso", {'shell':'powershell.exe'}, (error, data) => {
+    let executePowerShell = (commandInput, asRes = 0, res = null) => {
+        exec(commandInput, {'shell':'powershell.exe'}, (error, data) => {
             if(error) {
                 console.log(error);
+
             } else {
-                console.log(data)
-                data = JSON.parse(data)
-                res.status(200).json(data)
-                
-                //Envoyer la valeur à Angular pour que le bouton ait comme :
-                
-                //valeur à envoyer au script : \\Item1.Folder\Item1.Filename
-                
-                //valeur d'affichage du select: option1 = Item1.Filename     option2 = Item2.Filename 
+                try {
+                    data = JSON.parse(data)
+                    console.log(data);
+
+                    if(asRes && res != null) {
+                        res.status(200).json(data)
+                    }
+                }
+
+                catch {
+                    console.log(data);
+                }
             }
         });
+    }
+
+    router.get('/initcreation', async (req, res, next) => {
+        var commandInput = `Get-AvailableIso`
+        executePowerShell(commandInput, 1, res);
+
+        // let proc = exec("Get-AvailableIso", {'shell':'powershell.exe'}, (error, data) => {
+        //     if(error) {
+        //         console.log(error);
+        //     } else {
+        //         console.log(data)
+        //         data = JSON.parse(data)
+        //         res.status(200).json(data)
+        //     }
+        // });
     });
 
     router.post('/createvm', async (req, res, next) => {
-        console.log(req.body);
         var commandInput = `Add-NewVM -VMName ${req.body.vmName} -VMRam ${req.body.ram}${req.body.ramUnity} -VMDiskSize ${req.body.storage}${req.body.storageUnity} -VMOS ${req.body.os} -VMProcessor ${req.body.processor} -VirtualizationServer ${req.body.virtualizationServer}`
+        executePowerShell(commandInput, 1, res);
 
-        console.log(commandInput)
+        // let proc = exec(commandInput, {'shell':'powershell.exe'}, (error, data) => {
+        //     if(error) {
+        //         console.log(error);
+        //     } else {
+        //         data = data.split(/\r?\n/)
+        //         console.log(data)
 
-        let proc = exec(commandInput, {'shell':'powershell.exe'}, (error, data) => {
-            if(error) {
-                console.log(error);
-            } else {
-                data = data.split(/\r?\n/)
-                console.log(data)
+        //         data.forEach(element => {
+        //             if(element.includes('Id:')) {
+        //                 var vmId = element.replace('Id: ', '')
+        //                 console.log(vmId)
 
-                data.forEach(element => {
-                    if(element.includes('Id:')) {
-                        var vmId = element.replace('Id: ', '')
-                        console.log(vmId)
+        //                 res.json({
+        //                     vmId: vmId,
+        //                 })// Pour retourner des données après le POST /!\ angular attend du json /!\
 
-                        res.json({
-                            vmId: vmId,
-                        })// Pour retourner des données après le POST /!\ angular attend du json /!\
-
-                        //Enregistrer dans la bdd les infos de la vm avec son vmId
-                    }
-                });
-            }
-        });
+        //                 //Enregistrer dans la bdd les infos de la vm avec son vmId
+        //             }
+        //         });
+        //     }
+        // });
     });
 
     router.post('/deletevm', async (req, res, next) => {
-        var commandInput = `Uninstall-VM -VmId ${req.body.vmId} -VirtualizationServer ${req.body.virtualizationServer}`
-
-        console.log(commandInput)
-
-        let proc = exec(commandInput, {'shell':'powershell.exe'}, (error, data) => {
-            if(error) {
-                console.log(error);
-            } else {
-                console.log(data)
-
-                //Si data affiche un message d'erreur affirmant qu'il n'a pas trouvé la VM ne pas s'inquièter celà veut dire que la machine a bien été supprimée
-
-                //Supprimer les infos de la VM aussi dans la base de données
-            }
-        });
+        var commandInput = `Uninstall-VM -VmId ${req.body.vmId} -VirtualizationServerName ${req.body.virtualizationServer}`
+        executePowerShell(commandInput);
     });
 
-    router.get('/instance-monitoring', async (req, res, next) => {
-        var commandInput = `Get-MonitoringData -VMId ${req.body.instanceId} -VirtualizationServer ${req.body.virtualizationServer}`
+    router.get('/getvmstatus', async (req, res, next) => {
+        var commandInput = `Get-VMStatus -VMId ${req.body.vmId} -VirtualizationServerName ${req.body.virtualizationServer}`
+        executePowerShell(commandInput);
+    });
 
-        //Get-MonitoringMode -VMId ${req.body.instanceId}  => Voir si le monitoring est activé ou non sur la VM retourne un JSON en type String
-        
-        //Update-MonitoringMode -VMId ${req.body.instanceId} -isMonitored False -VirtualizationServer VMSRV01  => Activer/Désactiver le monitoring
+    router.get('/setvmstatus', async (req, res, next) => {
+        var commandInput = `Set-VMStatus -VMId ${req.body.vmId} -VirtualizationServerName ${req.body.virtualizationServer} -VMStatus ${req.body.status}`
+        executePowerShell(commandInput);
+    });
 
-        console.log(commandInput) 
+    router.get('/setvmconfig', async (req, res, next) => {
+        if(req.body.ram != null) {
+            var commandInput = `Update-VMMemory -VMId ${req.body.vmId} -VirtualizationServerName ${req.body.virtualizationServer} -NewVMRam ${req.body.ram}`
+            executePowerShell(commandInput);
+        }
 
-        let proc = exec(commandInput, {'shell':'powershell.exe'}, (error, data) => {
-            if(error) {
-                console.log(error);
+        if(req.body.cpu != null) {
+            var commandInput = `Update-VMVCPU -VMId ${req.body.vmId} -VirtualizationServerName ${req.body.virtualizationServer} -NewVMVCPU ${req.body.cpu}`
+            executePowerShell(commandInput);
+        }
 
-            } else {
-                console.log(data)
-            }
-        });
+        if(req.body.disk != null) {
+            var commandInput = `Expand-VMDiskSize -DiskPath ${req.body.disk} -VirtualizationServerName ${req.body.virtualizationServer} -SetMaxSize ${req.body.size}`
+            executePowerShell(commandInput);
+        }
+    });
+
+    //Ne fonctionne pas encore...
+    router.get('/getmonitoringmode', async (req, res, next) => {
+        var commandInput = `Get-MonitoringMode -VMId ${req.body.vmId}`
+        executePowerShell(commandInput);
+    });
+
+    //Ne fonctionne pas encore...
+    router.get('/setmonitoringmode', async (req, res, next) => {
+        var commandInput = `Update-MonitoringMode -VMId ${req.body.vmId} -isMonitored ${req.body.monitoring} -VirutalizationServerName ${req.body.virtualizationServer}`
+        executePowerShell(commandInput);
+    })
+
+    //Ne fonctionne pas encore...
+    router.get('/getmonitoringdata', async (req, res, next) => {
+        var commandInput = `Get-MonitoringData  -VMId ${req.body.vmId} -VirtualizationServerName ${req.body.virtualizationServer}`
+        executePowerShell(commandInput);
     });
 
     return router;
